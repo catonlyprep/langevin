4c93bd5515a0c1d6a1f85e4cecc2b584cd89f6d4
Tom Furnival <tjof2@cam.ac.uk>
2016-03-15 23:23:52 +0000

    Fixed further errors in .travis.yml

--- install-gsl.sh
+++ install-gsl.sh
@@ -2,4 +2,6 @@
 set -ex
 wget http://ftp.gnu.org/pub/gnu/gsl/gsl-2.1.tar.gz
 tar -xzvf gsl-2.1.tar.gz
-cd gsl-2.1 && ./configure --prefix=/usr --disable-static && make && sudo make install
+cd gsl-2.1 && ./configure --prefix=/usr --disable-static 
+make > gsl-log-file 2>&1
+sudo make install
