fbdd0b6a16e8008399a48c4f7368ce71efd1dd7c
Tom Furnival <tjof2@cam.ac.uk>
2016-03-15 23:02:46 +0000

    Added GSL-2.1 install script

---
+++ install-gsl.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -ex
+wget http://ftp.gnu.org/pub/gnu/gsl/gsl-2.1.tar.gz
+tar -xzvf gsl-2.1.tar.gz
+cd gsl-2.1 && ./configure --prefix=/usr --disable-static && make && sudo make install
